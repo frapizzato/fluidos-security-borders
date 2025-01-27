11265f996af80be87ebf1c8008fb0128c806f339
frapizzato <francesco.pizzato@polito.it>
2025-01-27 22:02:54 +0100

    Refactor of repo directories

--- gen-src/eu/fluidos/jaxb/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Questo file è stato generato dall'Eclipse Implementation of JAXB, v3.0.0 
-// Vedere https://eclipse-ee4j.github.io/jaxb-ri 
-// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2024.09.16 alle 02:11:49 PM CEST 
-//
-
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package eu.fluidos.jaxb;
